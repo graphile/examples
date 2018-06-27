da9165f600c2b6ee6025ce4fbcfd0fc16bbeb957
Benjie Gillam <benjie@jemjie.com>
2018-06-27 20:39:36 +0100

    Slightly style forums

--- client-react/src/components/HomePage.js
+++ client-react/src/components/HomePage.js
@@ -44,30 +44,32 @@ export default class HomePage extends React.Component {
     return (
       <Main>
         <h1>Forums</h1>
-        {allForums.nodes.length ? (
-          allForums.nodes.map(node => (
-            <ForumItem
-              key={node.nodeId}
-              forum={node}
-              currentUser={currentUser}
-            />
-          ))
-        ) : (
-          <div>
-            There are no forums yet!{" "}
-            {currentUser ? (
-              currentUser.isAdmin ? (
-                "Create one below..."
+        <div className="HomePage-forums">
+          {allForums.nodes.length ? (
+            allForums.nodes.map(node => (
+              <ForumItem
+                key={node.nodeId}
+                forum={node}
+                currentUser={currentUser}
+              />
+            ))
+          ) : (
+            <div>
+              There are no forums yet!{" "}
+              {currentUser ? (
+                currentUser.isAdmin ? (
+                  "Create one below..."
+                ) : (
+                  "Please check back later or contact an admin."
+                )
               ) : (
-                "Please check back later or contact an admin."
-              )
-            ) : (
-              <span>
-                Perhaps you need to <Link to="/login">log in</Link>?
-              </span>
-            )}
-          </div>
-        )}
+                <span>
+                  Perhaps you need to <Link to="/login">log in</Link>?
+                </span>
+              )}
+            </div>
+          )}
+        </div>
         {currentUser && currentUser.isAdmin ? (
           <div>
             <h2>Create new forum</h2>
