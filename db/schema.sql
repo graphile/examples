12e14cc07f66c3fdc1a7239509b9bc56126f2bf2
Benjie Gillam <benjie@jemjie.com>
2018-06-27 20:34:28 +0100

    Fix sequence permissions

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ create schema app_public;
 create schema app_private;
 
 grant usage on schema app_public to graphiledemo_visitor;
+alter default privileges in schema app_public grant usage, select on sequences to graphiledemo_visitor;
 
 --------------------------------------------------------------------------------
 
