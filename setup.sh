c217c6d2377a47a7e12c090d8994d5ab1c8b3f0b
Benjie Gillam <benjie@jemjie.com>
2018-07-03 10:00:06 +0100

    Tweak setup script

--- setup.sh
+++ setup.sh
@@ -3,20 +3,35 @@ set -e
 set -x
 export NODE_ENV=development
 
-# This will generate passwords that are safe to use in envvars without needing to be escaped:
-GENERATED_PASSWORD="$(openssl rand -base64 30 | tr '+/' '-_')"
-GENERATED_PASSWORD2="$(openssl rand -base64 30 | tr '+/' '-_')"
+if [ -x .env ]; then
+  . ./.env
+  if [ "$SUPERUSER_PASSWORD" = "" ]; then
+    echo ".env already exists, but it doesn't define SUPERUSER_PASSWORD - aborting!"
+    exit 1;
+  fi
+  if [ "$AUTH_USER_PASSWORD" = "" ]; then
+    echo ".env already exists, but it doesn't define AUTH_USER_PASSWORD - aborting!"
+    exit 1;
+  fi
+else
+  # This will generate passwords that are safe to use in envvars without needing to be escaped:
+  SUPERUSER_PASSWORD="$(openssl rand -base64 30 | tr '+/' '-_')"
+  AUTH_USER_PASSWORD="$(openssl rand -base64 30 | tr '+/' '-_')"
 
-# This is our '.env' config file, we're writing it now so that if something goes wrong we won't lose the passwords.
-cat >> .env <<CONFIG
-export ROOT_DATABASE_URL="postgresql://graphiledemo:$GENERATED_PASSWORD@localhost/graphiledemo"
-export AUTH_DATABASE_URL="postgresql://graphiledemo_authenticator:$GENERATED_PASSWORD2@localhost/graphiledemo"
-export TEST_ROOT_DATABASE_URL="postgresql://graphiledemo:$GENERATED_PASSWORD@localhost/graphiledemo_test"
-export TEST_AUTH_DATABASE_URL="postgresql://graphiledemo_authenticator:$GENERATED_PASSWORD2@localhost/graphiledemo_test"
+  # This is our '.env' config file, we're writing it now so that if something goes wrong we won't lose the passwords.
+  cat >> .env <<CONFIG
+export SUPERUSER_PASSWORD="$SUPERUSER_PASSWORD"
+export AUTH_USER_PASSWORD="$AUTH_USER_PASSWORD"
 export SECRET="$(openssl rand -base64 48)"
 export JWT_SECRET="$(openssl rand -base64 48)"
-export CLIENT_PORT=8350
+export ROOT_DATABASE_URL="postgresql://graphiledemo:\$SUPERUSER_PASSWORD@localhost/graphiledemo"
+export AUTH_DATABASE_URL="postgresql://graphiledemo_authenticator:\$AUTH_USER_PASSWORD@localhost/graphiledemo"
+export TEST_ROOT_DATABASE_URL="postgresql://graphiledemo:\$SUPERUSER_PASSWORD@localhost/graphiledemo_test"
+export TEST_AUTH_DATABASE_URL="postgresql://graphiledemo_authenticator:\$AUTH_USER_PASSWORD@localhost/graphiledemo_test"
+# This port is the one you'll connect to
 export PORT=8349
+# This is the port that create-react-app runs as, don't connect to it directly
+export CLIENT_PORT=8350
 export ROOT_DOMAIN="localhost:\$PORT"
 export ROOT_URL="http://\$ROOT_DOMAIN"
 export REDIS_URL="redis://localhost/3"
@@ -31,18 +46,19 @@ export GITHUB_KEY=""
 export GITHUB_SECRET=""
 CONFIG
 
-# To source our .env file from the shell it has to be executable.
-chmod +x .env
+  # To source our .env file from the shell it has to be executable.
+  chmod +x .env
+fi
 
 # Now we'll create the roles, databases and extensions in PostgreSQL:
 psql -X -v ON_ERROR_STOP=1 template1 <<SQL
 -- Ref: https://devcenter.heroku.com/articles/heroku-postgresql#connection-permissions
 
 -- This is the root role for the database
-CREATE ROLE graphiledemo WITH LOGIN PASSWORD '${GENERATED_PASSWORD}' SUPERUSER;
+CREATE ROLE graphiledemo WITH LOGIN PASSWORD '${SUPERUSER_PASSWORD}' SUPERUSER;
 
 -- This is the no-access role that PostGraphile will run as by default
-CREATE ROLE graphiledemo_authenticator WITH LOGIN PASSWORD '${GENERATED_PASSWORD2}' NOINHERIT;
+CREATE ROLE graphiledemo_authenticator WITH LOGIN PASSWORD '${AUTH_USER_PASSWORD}' NOINHERIT;
 
 -- This is the role that PostGraphile will switch to (from graphiledemo_authenticator) during a transaction
 CREATE ROLE graphiledemo_visitor;
