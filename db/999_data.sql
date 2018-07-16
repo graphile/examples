aa8233b3de2f791ab11dc40d8f1f2c6f6425e11c
Jem <jem@jemjie.com>
2018-07-16 11:01:17 +0100

    Topics now have body

--- db/999_data.sql
+++ db/999_data.sql
@@ -10,10 +10,10 @@ insert into app_public.forums(slug, name, description) values
   ('dog-life', 'Dog Life', ''),
   ('slug-life', 'Slug Life', '');
 
-insert into app_public.topics(forum_id, user_id, title) values
-  (1, 1, 'cats cats cats'),
-  (1, 1, 'snooze life'),
-  (1, 1, 'too hot');
+insert into app_public.topics(forum_id, user_id, title, body) values
+  (1, 1, 'cats cats cats', 'lets discuss cats because theyre totally cool'),
+  (1, 1, 'snooze life', 'do you find your cat just sleeps everywhere'),
+  (1, 1, 'too hot', 'its so hot my cat just flops where ever there is shade');
 
 insert into app_public.posts(topic_id, user_id, body) values
   (1, 1, 'Dont you just love cats? Cats cats cats cats cats cats cats cats cats cats cats cats Cats cats cats cats cats cats cats cats cats cats cats cats'),
