6a416a1b88abf9d55d7139edd0926e1b2642fc02
Connor Holyday <connorholyday@gmail.com>
2018-07-17 22:15:25 +0100

    Move reply timestamp to body

--- client-react/src/components/presentational/Reply.js
+++ client-react/src/components/presentational/Reply.js
@@ -3,14 +3,14 @@ import moment from "moment";
 
 export const Reply = ({ user, createdAt, body }) => (
   <article className="PostItem">
-    <div className="PostItem-meta">
-      <div className="PostItem-user PostItem-user--with-avatar">
-        <img alt="" className="PostItem-avatar" src={user.avatarUrl} />
-        {user.name}
-      </div>
-      <time className="PostItem-date">{moment(createdAt).calendar()}</time>
+    <div className="PostItem-meta PostItem-user PostItem-user--with-avatar">
+      <img alt="" className="PostItem-avatar" src={user.avatarUrl} />
+      {user.name}
     </div>
-    <p className="PostItem-body">{body}</p>
+    <div>
+      <time className="PostItem-date">{moment(createdAt).calendar()}</time>
+      <p className="PostItem-body">{body}</p>
+    </div>
   </article>
 );
 
