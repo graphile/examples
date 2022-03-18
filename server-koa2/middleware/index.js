653357c424e937a36380709dbbc1dfc0a4f5bc82
Tobias Speicher <rootcommander@gmail.com>
2022-03-18 12:02:31 +0100

    chore: replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- server-koa2/middleware/index.js
+++ server-koa2/middleware/index.js
@@ -4,7 +4,7 @@ const middlewares = fs
   .readdirSync(__dirname)
   .filter(fn => fn !== "index.js")
   .filter(fn => fn.match(/^[^.].*\.js$/))
-  .map(str => str.substr(0, str.length - 3));
+  .map(str => str.slice(0, -3));
 
 middlewares.forEach(name => {
   // eslint-disable-next-line import/no-dynamic-require
